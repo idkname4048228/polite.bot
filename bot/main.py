ece1230f1079240e40d79942ea6e5a23dc370343
kay <kay1314233@gmail.com>
2022-07-01 17:27:26 +0800

    新增對不起

--- bot/main.py
+++ bot/main.py
@@ -28,14 +28,15 @@ async def on_message(message):
                 await message.channel.send("謝謝大哥")
             time.sleep(1)
 
-
-        # # 分割訊息成兩份
-        # tmp = message.content.split(" ", 2)
-        # # 如果分割後串列長度只有1
-        # if len(tmp) == 1:
-        #     await message.channel.send("你要我說什麼啦？")
-        # else:
-        #     await message.channel.send(tmp[1])
+@client.event
+# 當有訊息時
+async def on_message(message):
+    # 排除自己的訊息，避免陷入無限循環
+    if message.author == client.user:
+        return
+    # 如果以「說」開頭
+    if message.content.startswith("對不起"):
+        await message.channel.send("對不起")
 
 
 TOKEN = os.getenv("DISCORD_TOKEN")
