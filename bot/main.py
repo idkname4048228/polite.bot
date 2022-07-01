afa4e10538a2601231733a29895f2c9bdacd3ac9
kay <kay1314233@gmail.com>
2022-07-01 17:05:36 +0800

    nothing

--- bot/main.py
+++ bot/main.py
@@ -1,19 +1,29 @@
-import os
-from discord.ext import commands
+#導入Discord.py
+import discord
+#client是我們與Discord連結的橋樑
+client = discord.Client()
 
-bot = commands.Bot(command_prefix="!")
-TOKEN = os.getenv("DISCORD_TOKEN")
-
-
-@bot.event
+#調用event函式庫
+@client.event
+#當機器人完成啟動時
 async def on_ready():
-    print(f"Logged in as {bot.user.name}({bot.user.id})")
+    print('目前登入身份：',client.user)
 
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send("pong")
-
+@client.event
+#當有訊息時
+async def on_message(message):
+    #排除自己的訊息，避免陷入無限循環
+    if message.author == client.user:
+        return
+    #如果以「說」開頭
+    if message.content.startswith('說'):
+      #分割訊息成兩份
+      tmp = message.content.split(" ",2)
+      #如果分割後串列長度只有1
+      if len(tmp) == 1:
+        await message.channel.send("你要我說什麼啦？")
+      else:
+        await message.channel.send(tmp[1])
 
 if __name__ == "__main__":
-    bot.run(TOKEN)
+    client.run('OTg3MTk4OTA2NDM1Nzc2NjEz.GgRbxN.ceaZNbDNVIrJ14XC01e_HJdCFEs4Klz7nDlP0g') #TOKEN在剛剛Discord Developer那邊「BOT」頁面裡面
