724ff7943bff53bafa2bb86cbd2a5f613676d688
kay <kay1314233@gmail.com>
2022-07-02 19:32:26 +0800

    完工

--- bot/main.py
+++ bot/main.py
@@ -14,39 +14,43 @@ async def on_ready():
     print("目前登入身份：", client.user)
     global times
     times = 0
-    
 
 
 async def thank_aniki(message, message_times):
-    if message.author == client.user or message_times > 1:
+    if message_times > 1:
         return
     while 1:
         await asyncio.sleep(1)
         now_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-        print(now_time)
-        if now_time[11:19:1] == "19:26:20":
+        print(now_time, times)
+        if now_time[11:19:1] == "22:00:00":
             await message.channel.send("謝謝大哥")
             await asyncio.sleep(86400 - 1)
 
-async def sumimasan(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith("對不起"):
-        await message.channel.send("對不起")
 
+async def sumimasan(message):
+    if (
+        message.content.startswith("對不起")
+        or message.content.startswith("sorry")
+        or message.content.startswith("我很抱歉")
+    ):
+        await message.channel.send("對不起")
+    elif message.content.startswith("time"):
+        await message.channel.send(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
 
 
 @client.event
 # 當有訊息時
 async def on_message(message):
     global times
+    if message.author == client.user:
+        return
     if times <= 1:
         times += 1
-    sorry = asyncio.create_task(sumimasan(message)) 
-    aniki = asyncio.create_task(thank_aniki(message)) 
+    sorry = asyncio.create_task(sumimasan(message))
+    aniki = asyncio.create_task(thank_aniki(message, times))
     await sorry
     await aniki
-    
 
 
 TOKEN = os.getenv("DISCORD_TOKEN")
